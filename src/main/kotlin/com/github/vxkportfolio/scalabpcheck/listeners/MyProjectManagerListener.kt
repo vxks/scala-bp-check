ac74ccbee6c1bc584732d4e02638c9fbb6279a44
GitHub Action <action@github.com>
2021-06-10 19:40:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/vxkportfolio/scalabpcheck/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/vxkportfolio/scalabpcheck/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vxkportfolio.scalabpcheck.listeners
 
+import com.github.vxkportfolio.scalabpcheck.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
